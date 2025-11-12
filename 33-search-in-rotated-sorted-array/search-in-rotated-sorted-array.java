eec929e3c2ee8979d85040590ffe0e37ed4f7f47
Madhusiddani <158128476+Madhusiddani@users.noreply.github.com>
2025-11-12 10:48:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (8.58%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
+        // O(n) complexity linear search
         // for (int i = 0; i < nums.length; i++) {
         //     if (nums[i] == target) {
         //         return i;
@@ -7,19 +8,48 @@ class Solution {
         // }
         // return -1;
 
+
+// binary search with complexity O(n)
+        // int l=0;
+        // int r=nums.length-1;
+        // while(l<=r){
+        //     if(nums[l]==target){
+        //         return l;
+        //     }
+        //     if(nums[r]==target){
+        //         return r;
+        //     }else{
+        //         l++;
+        //         r--;
+        //     }
+        // }
+        // return -1;
+
+
+        // Using pivot element to get complexity O(log n)
+
         int l=0;
         int r=nums.length-1;
         while(l<=r){
-            if(nums[l]==target){
-                return l;
+            int mid = l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            if(nums[r]==target){
-                return r;
-            }else{
-                l++;
-                r--;
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<= target && target< nums[mid]){
+                    r = mid -1;
+                }else{
+                    l = mid + 1;
+                }
             }
-        }
+            else{
+                if(nums[mid]< target && target <= nums[r]){
+                    l = mid+1;
+                }else{
+                    r = mid -1;
+                }
+            }
+        } 
         return -1;
     }
 }
