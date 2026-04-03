cd41980f1c46c1981f379ec07335be69014af2c6
Madhusiddani <158128476+Madhusiddani@users.noreply.github.com>
2026-04-03 10:09:45 +0530

    Time: 102 ms (100.00%) | Memory: 143.9 MB (60.00%) - LeetSync

--- 3982-maximum-walls-destroyed-by-robots/maximum-walls-destroyed-by-robots.java
+++ 3982-maximum-walls-destroyed-by-robots/maximum-walls-destroyed-by-robots.java
@@ -1,102 +1,100 @@
+import java.util.*;
+
 class Solution {
 
-    private long[][] arr;
-    private int[][] dp;
-    private int[] walls;
-    private int n;
+    static final int INF = Integer.MAX_VALUE;
+    static final int NEG_INF = Integer.MIN_VALUE;
 
-    public int maxWalls(int[] pos, int[] dist, int[] w) {
+    static class Node {
+        int p, d;
+        Node(int p, int d) {
+            this.p = p;
+            this.d = d;
+        }
+    }
 
-        n = pos.length;
-        if (n == 0) return 0;
+    public int maxWalls(int[] robots, int[] distance, int[] walls) {
+        int n = robots.length;
 
-        walls = w;
+        Node[] arr = new Node[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Node(robots[i], distance[i]);
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt(a -> a.p));
         Arrays.sort(walls);
 
-        long[][] a = new long[n][2];
+        return solve(arr, n, walls);
+    }
+
+    private int solve(Node[] arr, int n, int[] walls) {
+        int[] dpL = new int[n];
+        int[] dpR = new int[n];
+
+        int[] left = buildLeft(arr, n, walls);
+        int[] right = buildRight(arr, n, walls);
+        int[] common = buildCommon(arr, n, walls);
+
+        dpL[0] = left[0];
+        dpR[0] = right[0];
+
+        for (int i = 1; i < n; i++) {
+            dpL[i] = left[i] + Math.max(dpL[i - 1], dpR[i - 1] - common[i]);
+            dpR[i] = right[i] + Math.max(dpL[i - 1], dpR[i - 1]);
+        }
+
+        return Math.max(dpL[n - 1], dpR[n - 1]);
+    }
+
+    private int[] buildLeft(Node[] arr, int n, int[] walls) {
+        int[] res = new int[n];
+        int l = 0, r = -1, m = walls.length;
+
         for (int i = 0; i < n; i++) {
-            a[i][0] = pos[i];
-            a[i][1] = dist[i];
+            int prev = (i == 0) ? NEG_INF : arr[i - 1].p;
+            int s = Math.max(prev + 1, arr[i].p - arr[i].d);
+            int e = arr[i].p;
+
+            while (r + 1 < m && walls[r + 1] <= e) r++;
+            while (l < m && walls[l] < s) l++;
+
+            if (l <= r) res[i] = r - l + 1;
         }
+        return res;
+    }
 
-        Arrays.sort(a, (x, y) -> Long.compare(x[0], y[0]));
-
-        arr = new long[n][4];
+    private int[] buildRight(Node[] arr, int n, int[] walls) {
+        int[] res = new int[n];
+        int l = 0, r = -1, m = walls.length;
 
         for (int i = 0; i < n; i++) {
+            int next = (i == n - 1) ? INF : arr[i + 1].p;
+            int s = arr[i].p;
+            int e = Math.min(next - 1, arr[i].p + arr[i].d);
 
-            long p = a[i][0];
-            long d = a[i][1];
+            while (r + 1 < m && walls[r + 1] <= e) r++;
+            while (l < m && walls[l] < s) l++;
 
-            long left = (i > 0 ? a[i - 1][0] : Long.MIN_VALUE);
-            long right = (i < n - 1 ? a[i + 1][0] : Long.MAX_VALUE);
-
-            arr[i][0] = Math.max(p - d, left);
-            arr[i][1] = p;
-            arr[i][2] = p;
-            arr[i][3] = Math.min(p + d, right);
+            if (l <= r) res[i] = r - l + 1;
         }
-
-        dp = new int[n][3];
-        for (int[] x : dp) Arrays.fill(x, -1);
-
-        return f(0, 2);
+        return res;
     }
 
-    private int f(int i, int prev) {
+    private int[] buildCommon(Node[] arr, int n, int[] walls) {
+        int[] res = new int[n];
+        int l = 0, r = -1, m = walls.length;
 
-        if (i == n) return 0;
+        for (int i = 1; i < n; i++) {
+            int s = Math.max(arr[i - 1].p + 1, arr[i].p - arr[i].d);
+            int e = Math.min(arr[i].p - 1, arr[i - 1].p + arr[i - 1].d);
 
-        if (dp[i][prev] != -1) return dp[i][prev];
+            if (s > e) continue;
 
-        long last = Long.MIN_VALUE;
+            while (r + 1 < m && walls[r + 1] <= e) r++;
+            while (l < m && walls[l] < s) l++;
 
-        if (prev == 0) last = arr[i - 1][1];
-        else if (prev == 1) last = arr[i - 1][3];
-
-        int res = 0;
-
-        // left
-        long l = arr[i][0], r = arr[i][1];
-        int c1 = cnt(Math.max(l, last + 1), r);
-        res = Math.max(res, c1 + f(i + 1, 0));
-
-        // right
-        l = arr[i][2];
-        r = arr[i][3];
-        int c2 = cnt(Math.max(l, last + 1), r);
-        res = Math.max(res, c2 + f(i + 1, 1));
-
-        return dp[i][prev] = res;
-    }
-
-    private int cnt(long l, long r) {
-
-        if (l > r) return 0;
-
-        int i1 = lb(walls, l);
-        int i2 = ub(walls, r);
-
-        return i2 - i1;
-    }
-
-    private int lb(int[] a, long t) {
-        int l = 0, r = a.length;
-        while (l < r) {
-            int m = (l + r) / 2;
-            if (a[m] >= t) r = m;
-            else l = m + 1;
+            if (l <= r) res[i] = r - l + 1;
         }
-        return l;
-    }
-
-    private int ub(int[] a, long t) {
-        int l = 0, r = a.length;
-        while (l < r) {
-            int m = (l + r) / 2;
-            if (a[m] > t) r = m;
-            else l = m + 1;
-        }
-        return l;
+        return res;
     }
 }
